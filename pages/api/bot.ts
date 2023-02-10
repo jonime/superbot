fb27354a4c14871fcb6a13f882d73fce85615218
Joni Mertoniemi <joni.mertoniemi@gmail.com>
2023-02-10 23:14:23 +0200

    Update bot

---
+++ pages/api/bot.ts
@@ -0,0 +1,35 @@
+import { App, SlackEvent } from "@slack/bolt";
+import { NextApiHandler, NextApiResponse } from "next";
+
+const app = new App({
+  clientId: process.env.SLACK_APP_CLIENT_ID,
+  clientSecret: process.env.SLACK_APP_CLIENT_SECRET,
+  signingSecret: process.env.SLACK_APP_SIGN_SECRET,
+  token: process.env.SLACK_APP_TOKEN,
+});
+
+const handleEvent = async (event: SlackEvent, res: NextApiResponse) => {
+  switch (event.type) {
+    case "message":
+      if ("bot_id" in event) {
+        return;
+      }
+      if ("text" in event && event.text === "nakki") {
+        await app.client.chat.postMessage({
+          channel: event.channel,
+          text: "nakki",
+        });
+      }
+  }
+};
+
+const handler: NextApiHandler = async (req, res) => {
+  if (req.body.type === "url_verification") {
+    res.status(200).send(req.body.challenge);
+  } else if (req.body.type === "event_callback") {
+    await handleEvent(req.body.event, res);
+    res.status(200).send("OK");
+  }
+};
+
+export default handler;
